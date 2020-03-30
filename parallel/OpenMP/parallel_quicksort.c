734932a825c6eb7cbd2e4174911f0113cf2ffcb4
Haya <Hayaa@taibahu.edu.sa>
2020-03-30 22:26:54 +0300

    Allocation bug fix

--- parallel/OpenMP/parallel_quicksort.c
+++ parallel/OpenMP/parallel_quicksort.c
@@ -64,19 +64,18 @@ int main(int argc, char* argv[]) {
 		}
 	} else {			/*	normal execution	*/ 
 		a = (int*) (malloc(n * sizeof(int)));
-		for (int i = 0; i < 1000; i++) {
-
+		//for (int i = 0; i < 1000; i++) { 
 			rand_arr_gen(a,n);
 			
 			start = omp_get_wtime();
-			parallel_quicksort(b, n, thread_count);
+			parallel_quicksort(a, n, thread_count);
 			elapsed = omp_get_wtime() - start;
 
-			// printf("Parallel time: %f milliseconds\n", (elapsed * 1000));
+			printf("Parallel time: %f milliseconds\n", (elapsed * 1000));
 			if (verify(a, n) == 0) {
 				printf("failed\n");
 			} // non-parallel validation for the result
-		}
+		//}
 
 		free(a);
 	}
