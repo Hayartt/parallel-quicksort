d0ee1c2d25a91a717d23c6fe2d79e0591235fa9b
hotaf <32969357+hotaf@users.noreply.github.com>
2020-04-03 21:15:35 +0300

    Update QuikSortThreads.java

--- parallel/Java-multithreading/QuikSortThreads.java
+++ parallel/Java-multithreading/QuikSortThreads.java
@@ -1,25 +1,49 @@
 
 package quiksortthreads;
-import java.io.Console;
 import java.util.Random;
 
 public class QuikSortThreads implements Runnable{
+    
         int[] array;
         int My_start,My_end;
+        
      QuikSortThreads(int[] data,int start,int end) {
             this.array=data;
             this.My_start=start;
             this.My_end=end;
         }
+     
+        @Override
         public void run(){
             quickSort(this.array, this.My_start, this.My_end);
+                
+           /* To get the current thread id run the following */
+           /*  long ThreadID= Thread.currentThread().getId()%2 +1;
+             System.out.println("Thread " + ThreadID+ " of "+ 2 );*/
+                
         }
-        static void swap(int[] arr, int i, int j){
+/*-------------------------------------------------------------------
+ * Function:  swap
+ * Purpose:   swaps between two indices in an array	
+ * In args:    arr:	The array to get swapped
+               i:	First index
+               j:	Second index
+ *           
+ */ 
+       private static void swap(int[] arr, int i, int j){
             int tmp=arr[i];
             arr[i]=arr[j];
             arr[j]=tmp;
         }
-        static int partition(int[] arr, int start, int end) {
+/*-------------------------------------------------------------------
+ * Function:  partition
+ * Purpose:   Splits an array and swaps based on the pivot 	
+ * In args:    arr: The array to be sorted
+               start: First index in the array
+               end:   Last index in the array
+ *           
+ */       
+       private static int partition(int[] arr, int start, int end) {
             if(start==end)
                 return start;
             int pivot = arr[end];
@@ -30,14 +54,29 @@ public class QuikSortThreads implements Runnable{
             swap(arr,++s,end);
             return s;
         }
-        static void quickSort(int[] arr, int start, int end) {
+/*-------------------------------------------------------------------
+ * Function:  quickSort
+ * Purpose:   Splits an array and call quickSort method for the two parts	
+ * In args:    arr: The array to be sorted
+               start: First index in the array
+               end:   Last index in the array
+ *           
+ */
+       private static void quickSort(int[] arr, int start, int end) {
             if (end <= start)
                 return;
             int s = partition (arr,start,end);
             quickSort(arr,start,s-1);
             quickSort(arr,s+1,end);
         }
-        static int[] randomList(int n,int k) {
+
+/*-------------------------------------------------------------------
+ * Function:   randomList
+ * Purpose:    Generates n random integer values to fill an array.	
+ * In args:    n:	The size of an array
+ * 	       k:	The range of random numbers    
+ */
+    private static int[] randomList(int n,int k) {
             Random random=new Random();
             int[] RandomArray=new int[n];
             for(int i=0;i<RandomArray.length;i++)
@@ -45,34 +84,54 @@ public class QuikSortThreads implements Runnable{
             return RandomArray;
         }
 
+/*-------------------------------------------------------------------
+ * Function:   isSorted
+ * Purpose:   Verifies that an array is sorted in ascending order	
+ * In args:    n:	The array to be verified
+ *           
+ */
+   private   static boolean isSorted(int[] a) {
+		for (int i = 0; i < a.length - 1; i++) {
+			if (a[i] > a[i + 1]) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+/*------------------------------------------------------------------*/
+      
      public static void main(String[] args) {
              
-            int[] Array={10,1,25,2,20,6};
-            quickSort(Array,0,Array.length-1);
-                     
-            for(int i:Array)
-            System.out.println(i);
-
+            int[] Array;
             int n=10000000;
             Array=randomList(n,1000000);
             
             int s = partition(Array,0,n-1);
             
-            long startTime = System.currentTimeMillis(); 
+            long startTime = System.currentTimeMillis();
 
             Thread t1=new Thread(new QuikSortThreads(Array,0,s-1));
             Thread t2=new Thread(new QuikSortThreads(Array,s+1,Array.length-1));
             t1.start();
             t2.start();
+            
             try {
                 t1.join();
                 t2.join();
             }catch(InterruptedException e){
                 System.out.println(e);
             }
+            
             long endTime = System.currentTimeMillis();
-System.out.println("\n"); 
-System.out.println("Number of threads is " + 2 ); 
-System.out.println("Time with " + (endTime - startTime) + " milliseconds");
+            
+           boolean check = isSorted(Array);
+           if (check == false){
+                System.out.println("Failed");}
+
+          System.out.println("----------------------------------"); 
+          System.out.println("Number of threads is " + 2); 
+          System.out.println("Time with " + (endTime - startTime) + " milliseconds");
         }
     }
